da3159f4d58aa4dc2fc01c79717904e2c3ee5a81
Bryan Dollery <bryan.dollery@gmail.com>
2020-05-06 13:01:55 +0300

    starting point

---
+++ src/test/java/org/springframework/samples/petclinic/PetClinicSeleniumTestRunner.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PetClinicSeleniumTestRunner {
+    @Test
+    public void start() {
+        assertTrue(true);
+    }
+}
