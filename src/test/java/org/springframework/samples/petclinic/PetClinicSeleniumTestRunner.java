4201cb9c170a8c95e34f874181c45d1cecac3721
Bryan Dollery <bryan.dollery@gmail.com>
2020-05-06 13:53:43 +0300

    Added selenium to pom. Created empty Home POM class and steps. Renamed feature to home. Added annotations to junit class to run cucumber/selenium tests

--- src/test/java/org/springframework/samples/petclinic/PetClinicSeleniumTestRunner.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicSeleniumTestRunner.java
@@ -1,12 +1,25 @@
 package org.springframework.samples.petclinic;
 
-import org.junit.Test;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertTrue;
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+import static java.util.logging.Level.SEVERE;
+import static java.util.logging.Logger.getLogger;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "summary"},
+        strict = true,
+        snippets = CAMELCASE,
+        tags = "not @wip",
+        features = "src/test/resources")
 public class PetClinicSeleniumTestRunner {
-    @Test
-    public void start() {
-        assertTrue(true);
+
+    static {
+        System.setProperty("webdriver.chrome.driver", "/chromedriver.exe");
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+        getLogger("org.openqa.selenium").setLevel(SEVERE);
     }
+
 }
